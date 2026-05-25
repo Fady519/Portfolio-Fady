992dfbcfb2fd9ebac2ee89f0c6ce9777bf939d9f
Fady Kaiser <fadykaiser2004@gmail.com>
2026-05-25 10:38:47 +0300

    Updates

--- _next/static/UkwpHGrjbX2ALZynv41kd/_buildManifest.js
+++ _next/static/UkwpHGrjbX2ALZynv41kd/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Portfolio-Fady//_next/:path+",
-        "destination": "/Portfolio-Fady/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
