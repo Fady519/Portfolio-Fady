0736459aa12bf284a96ba51cbe749e5085986822
Fady Kaiser <fadykaiser2004@gmail.com>
2026-05-25 10:57:22 +0300

    Updates

--- _next/static/Mnib-NTlebTNB9v9KE69D/_buildManifest.js
+++ _next/static/Mnib-NTlebTNB9v9KE69D/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portfolio-Fady//_next/:path+",
+        "destination": "/Portfolio-Fady/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
